94682f5a93439742cbc6906f15e093eb95aec846
danielthirtle <drthirtle@gmail.com>
2024-09-07 00:38:33 +1200

    fix mult

--- osu.Game.Rulesets.Osu/Difficulty/OsuDifficultyCalculator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/OsuDifficultyCalculator.cs
@@ -22,7 +22,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty
 {
     public class OsuDifficultyCalculator : DifficultyCalculator
     {
-        private const double difficulty_multiplier = 0.0675;
+        private const double difficulty_multiplier = 0.068;
 
         public override int Version => 20220902;
 
